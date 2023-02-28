7f480ea63157d9be0293ea6f016623824582398c
aidnrm <89261263+aidnrm@users.noreply.github.com>
2023-02-28 11:56:41 -0500

    comments and syntax

--- main.py
+++ main.py
@@ -53,44 +53,11 @@ class ChessBoard(arcade.Window):
 
         # TODO: Call draw() on all your sprite 
 
-    def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
-
     def on_key_press(self, key, key_modifiers):
         """ Called whenever a key on the keyboard is pressed. """
         # Exit 
         if key == arcade.key.ESCAPE:
             arcade.close_window()
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
 
 def main():
     """ Main function """
