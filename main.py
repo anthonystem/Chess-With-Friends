07c780b955410254c7fee6a56669b726760307cb
aidnrm <89261263+aidnrm@users.noreply.github.com>
2023-03-06 16:18:12 -0500

    setting up menus

--- main.py
+++ main.py
@@ -12,9 +12,32 @@ BOARD_SIZE = 8
 MARGIN = 50
 
 
-class Board(arcade.View):
-    """ Main application class that opens window """
+class StartMenu(arcade.View):
+    """Create start menu """
 
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer / initialize constants
+        super().__init__()
+        self.width = SCREEN_WIDTH
+        self.height = SCREEN_HEIGHT
+        self.title = SCREEN_TITLE
+    
+    def on_show(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+        arcade.set_viewport(0, self.window.width, 0, self.window.height)
+    
+
+class HelperMenu(arcade.View):
+    """Helper menu explains game and controls """
+
+
+class Board(arcade.View):
+    """ Draws Board """
 
     def __init__(self):
         """ Initializer """
@@ -59,7 +82,7 @@ def main():
     """ Main method """
 
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    start_view = Board()
+    start_view = StartMenu()
     window.show_view(start_view)
     start_view.setup()
     arcade.run()
