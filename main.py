4c80d16d14a63a5bb383218c63a8bda113c48f86
aidnrm <amccmack@gmail.com>
2023-03-09 16:34:45 -0500

    organizing / figuring out moving pieces

--- main.py
+++ main.py
@@ -11,6 +11,111 @@ SQUARE_SIZE = 100
 BOARD_SIZE = 8
 MARGIN = 50
 
+class Board(arcade.View):
+    """ Draws Board and currently holds functionality of pieces"""
+
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer / initialize constants
+        super().__init__()
+        self.width = SCREEN_WIDTH
+        self.height = SCREEN_HEIGHT
+        self.title = SCREEN_TITLE
+        arcade.set_background_color(arcade.color.LIGHT_GRAY)
+
+        # List of pieces we are dragging with the mouse
+        self.held_pieces = None
+
+        # Original location of piece we are dragging with the mouse in case
+        # they have to go back.
+        self.held_pieces_original_position = None
+
+        # Generate pieces
+        self.king_b = arcade.Sprite("sprites/kingb.png", center_x= 350, center_y= 50)
+        self.queen_b = arcade.Sprite("sprites/queenb.png", center_x= 450, center_y= 50)
+        self.rook_b = arcade.Sprite("sprites/rookb.png", center_x= 750, center_y= 50)
+        self.rook_b2 = arcade.Sprite("sprites/rookb.png", center_x= 50, center_y= 50)
+        self.bishop_b = arcade.Sprite("sprites/bishopb.png", center_x= 250, center_y= 50)
+        self.bishop_b2 = arcade.Sprite("sprites/bishopb.png", center_x= 550, center_y= 50)
+        self.knight_b = arcade.Sprite("sprites/knightb.png", center_x= 150, center_y= 50)
+        self.knight_b2 = arcade.Sprite("sprites/knightb.png", center_x= 650, center_y= 50)
+        
+        self.pieces_list = arcade.SpriteList()
+
+    def setup(self):
+        """ Setup game here. Function should restart game """
+
+        # List of pieces we are dragging with the mouse
+        self.held_pieces = []
+
+        # Original location of piece we are dragging with the mouse in case
+        # they have to go back.
+        self.held_pieces_original_position = []
+
+        # Sprite list with all the pieces
+        self.pieces_list = arcade.SpriteList()
+
+        # Add pieces to list of pieces
+        self.pieces_list.append(self.king_b)
+        self.pieces_list.append(self.queen_b)
+        self.pieces_list.append(self.rook_b)
+        self.pieces_list.append(self.rook_b2)
+        self.pieces_list.append(self.bishop_b)
+        self.pieces_list.append(self.bishop_b2)
+        self.pieces_list.append(self.knight_b)
+        self.pieces_list.append(self.knight_b2)
+
+    def on_draw(self):
+        """
+        Render the board.
+        """
+        self.clear()
+
+        arcade.start_render()
+
+        # Iterate over each row and column
+        for row in range(BOARD_SIZE):
+            for column in range(BOARD_SIZE):
+
+                # Formula to calculate the x, y position of the square
+                x = column * SQUARE_SIZE + MARGIN
+                y = row * SQUARE_SIZE + MARGIN
+
+                # if square is even, draw a black rectangle
+                if (row + column) % 2 == 0:
+                    arcade.draw_rectangle_filled(x, y, SQUARE_SIZE, SQUARE_SIZE, arcade.color.ONYX)
+                else:
+                    arcade.draw_rectangle_filled(x, y, SQUARE_SIZE, SQUARE_SIZE, arcade.color.EGGSHELL)
+
+        # Render drawing after board
+        self.king_b.draw()
+        self.queen_b.draw()
+        self.rook_b.draw()
+        self.rook_b2.draw()
+        self.bishop_b.draw()
+        self.bishop_b2.draw()
+        self.knight_b.draw()
+        self.knight_b2.draw()
+        
+    
+    def on_mouse_press(self, x, y, button, modifiers):
+        """ Called when the user presses a mouse button. """
+        
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.king_b.center_x = x
+            self.king_b.center_y = y
+
+    
+    def on_key_press(self, key, key_modifiers):
+        """ Called whenever a key on the keyboard is pressed. """
+        # Exit 
+        if key == arcade.key.ESCAPE:
+            arcade.close_window()
+
+        if key == arcade.key.BACKSPACE:
+            game_view = StartMenu()
+            self.window.show_view(game_view)
+            arcade.run()
 
 class StartMenu(arcade.View):
     """Create start menu """
@@ -52,81 +157,6 @@ class StartMenu(arcade.View):
 class HelperMenu(arcade.View):
     #TODO: Implement this quickly so we have full functionality of menu related stuff.
     """Helper menu explains game and controls """
-
-
-class Board(arcade.View):
-    """ Draws Board """
-
-    def __init__(self):
-        """ Initializer """
-        # Call the parent class initializer / initialize constants
-        super().__init__()
-        self.width = SCREEN_WIDTH
-        self.height = SCREEN_HEIGHT
-        self.title = SCREEN_TITLE
-        arcade.set_background_color(arcade.color.LIGHT_GRAY)
-
-        #TODO: Sprites
-
-        self.king_b = arcade.Sprite("sprites/kingb.png", center_x= 350, center_y= 50)
-        self.queen_b = arcade.Sprite("sprites/queenb.png", center_x= 450, center_y= 50)
-        self.rook_b = arcade.Sprite("sprites/rookb.png", center_x= 750, center_y= 50)
-        self.rook_b2 = arcade.Sprite("sprites/rookb.png", center_x= 50, center_y= 50)
-        self.bishop_b = arcade.Sprite("sprites/bishopb.png", center_x= 250, center_y= 50)
-        self.bishop_b2 = arcade.Sprite("sprites/bishopb.png", center_x= 550, center_y= 50)
-        self.knight_b = arcade.Sprite("sprites/knightb.png", center_x= 150, center_y= 50)
-        self.knight_b2 = arcade.Sprite("sprites/knightb.png", center_x= 650, center_y= 50)
-
-
-    def on_draw(self):
-        """
-        Render the board.
-        """
-        self.clear()
-
-        arcade.start_render()
-
-        # Iterate over each row and column
-        for row in range(BOARD_SIZE):
-            for column in range(BOARD_SIZE):
-
-                # Formula to calculate the x, y position of the square
-                x = column * SQUARE_SIZE + MARGIN
-                y = row * SQUARE_SIZE + MARGIN
-
-                # if square is even, draw a black rectangle
-                if (row + column) % 2 == 0:
-                    arcade.draw_rectangle_filled(x, y, SQUARE_SIZE, SQUARE_SIZE, arcade.color.ONYX)
-                else:
-                    arcade.draw_rectangle_filled(x, y, SQUARE_SIZE, SQUARE_SIZE, arcade.color.EGGSHELL)
-
-        # Render drawing after board
-        self.king_b.draw()
-        self.queen_b.draw()
-        self.rook_b.draw()
-        self.rook_b2.draw()
-        self.bishop_b.draw()
-        self.bishop_b2.draw()
-        self.knight_b.draw()
-        self.knight_b2.draw()
-        
-    
-    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
-        """ Called when the user presses a mouse button. """
-        # TODO Functionality for pressing on sprites 
-        
-        piece = arcade.get_sprites_at_point((x,y))
-
-    def on_key_press(self, key, key_modifiers):
-        """ Called whenever a key on the keyboard is pressed. """
-        # Exit 
-        if key == arcade.key.ESCAPE:
-            arcade.close_window()
-
-        if key == arcade.key.BACKSPACE:
-            game_view = StartMenu()
-            self.window.show_view(game_view)
-            arcade.run()
 
 def main():
     """ Main method """
