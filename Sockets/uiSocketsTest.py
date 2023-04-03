29a8e4c3d1c663702248feaa58e936c9e188e24b
ihseh <hmuntiu@gmail.com>
2023-04-03 15:31:36 -0400

    Worked on socket testing UI to make input boxes compatible

--- Sockets/uiSocketsTest.py
+++ Sockets/uiSocketsTest.py
@@ -6,8 +6,8 @@ import threading
 import sys
 
 #arcade variables
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
 SCREEN_TITLE = "UI Test"
 SQUARE_SIZE = 100
 BOARD_SIZE = 8
@@ -42,7 +42,7 @@ def wait_for_server_input(client):
 #Buttons
 class InviteButton(arcade.gui.UIFlatButton):
     def on_click(self, event: arcade.gui.UIOnClickEvent):
-        send(f"{clientName},INVITE,aiden", client)
+        send(f"{clientName},INVITE,{game_view.input_box.text}", client)
 
 class AcceptButton(arcade.gui.UIFlatButton):
     def on_click(self, event: arcade.gui.UIOnClickEvent):
@@ -56,8 +56,6 @@ class InputBox(arcade.gui.UIInputText):
     def on_click(self, event: arcade.gui.UIOnClickEvent):
         pass
 
-inputText = ""
-
 #Home view
 class Home(arcade.View):
     def __init__(self):
@@ -68,14 +66,14 @@ class Home(arcade.View):
         self.manager.enable()
         arcade.set_background_color(arcade.csscolor.GRAY)
         self.v_box = arcade.gui.UIBoxLayout()
-        invite_button = InviteButton(text="Invite Player", width=400)
-        accept_button = AcceptButton(text="Accept Invite", width=400)
-        reject_button = RejectButton(text="Reject Invite", width=400)
-        input_box = InputBox(text = "Input player name", width = 400)
-        self.v_box.add(invite_button)
-        self.v_box.add(accept_button)
-        self.v_box.add(reject_button)
-        self.v_box.add(input_box)
+        self.invite_button = InviteButton(text="Invite Player", width=400)
+        self.accept_button = AcceptButton(text="Accept Invite", width=400)
+        self.reject_button = RejectButton(text="Reject Invite", width=400)
+        self.input_box = InputBox(text = "", width = 200)
+        self.v_box.add(self.invite_button)
+        self.v_box.add(self.input_box)
+        self.v_box.add(self.accept_button)
+        self.v_box.add(self.reject_button)
         self.manager.add(
             arcade.gui.UIAnchorWidget(
                 anchor_x="center_x",
@@ -88,19 +86,23 @@ class Home(arcade.View):
         arcade.start_render()
         self.clear()
         self.manager.draw()
-        
+
+
+#DEFINE GLOBALLY
+window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+game_view = Home()
 
 def main():
 
     #socket functionality
-    # client.connect(ADDR) #connect to server
-    # send(clientName, client) #send client name to server
-    # thread = threading.Thread(target = wait_for_server_input, args = [client])
-    # thread.start()
+    client.connect(ADDR) #connect to server
+    send(clientName, client) #send client name to server
+    thread = threading.Thread(target = wait_for_server_input, args = [client])
+    thread.start()
 
     #Arcade functionality
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game_view = Home()
+    # window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    # game_view = Home()
     window.show_view(game_view)
 
     arcade.run()
