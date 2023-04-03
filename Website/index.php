3ba0c08b9d487ec3b8c177737d5a713a71931c0a
anthonystem <anthony_stem@outlook.com>
2023-04-02 22:35:35 -0400

    Added website to sprint 2 results.

---
+++ Website/index.php
@@ -0,0 +1,45 @@
+<?php
+    include "top.php";
+?>
+
+        <main class="landing">
+            <div class="hero-banner">
+                <section class="hero-content">
+                    <h2>Chess With Friends</h2>
+                    <p><span class="emphasize">Connect</span> with your friends. <span class="emphasize">Play</span> chess. <span class="emphasize">Win</span>.</p>
+                    <nav class="cta-nav">
+                        <p><a href="register.php" class="cta">Register &amp; Play</a></p>
+                        <p><a href="#about" class="cta secondary-cta">Learn More</a></p>
+                    </nav>
+                </section>
+            </div>
+
+            <section class="about" id="about">
+                <h2>Placeholder for Descriptive/About Content (If Project Time Permits).</h2>
+                <p>The "Learn More" CTA button will just scroll the user down to this point.</p>
+            </section>
+        </main>
+
+        <?php
+            include "footer.php";
+        ?>
+
+        <script type="text/javascript">
+
+            // Handle Navigation Hamburger Button.
+            const hamburgerButton = document.querySelector(".hamburger-button");
+            const hamburgerCloseButton = document.querySelector(".hamburger-close-button");
+            const navigationLinks = document.querySelector(".navigation-links");
+
+            hamburgerButton.addEventListener('click', () => {
+                navigationLinks.classList.toggle("show");
+                hamburgerCloseButton.classList.toggle("show");
+            });
+
+            hamburgerCloseButton.addEventListener('click', () => {
+                navigationLinks.classList.toggle("show");
+                hamburgerCloseButton.classList.toggle("show");
+            });
+        </script>
+    </body>
+</html>
