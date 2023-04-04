0029e97ffa08dfc0ec186d9a8293c60f044b61ea
ihseh <hmuntiu@gmail.com>
2023-04-04 19:19:23 -0400

    socket testing with ui structure

--- Sockets/server.py
+++ Sockets/server.py
@@ -27,6 +27,16 @@ class Game:
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #choose socket family and type
 server.bind(ADDR) #bind server to address
 
+#send
+def send(msg, client):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
 #handles individual connection between client and server
 def handle_client(conn, addr):
 	print(f"[NEW CONNECTION] {addr} connected.")
@@ -41,7 +51,7 @@ def handle_client(conn, addr):
 				print("Disconected")
 			else:
 				print(f"[{addr}] {msg}")
-				conn.send("Message recieved\n".encode(FORMAT))
+				# conn.send("Message recieved\n".encode(FORMAT))
 				process(conn, msg)
 	
 	conn.close() #close current connection after client has disconnected
@@ -65,7 +75,7 @@ def process(sock, msg): #socket object, message
 			playerDic[spec[0]] = Player(spec[0],sock)
 		elif spec[0] in playerDic: #update player socket if player is reconnecting
 			playerDic[spec[0]].sock = sock
-			playerDic[spec[0]].sock.send("Updated Connection".encode(FORMAT))
+			# playerDic[spec[0]].sock.send("Updated Connection".encode(FORMAT))
 	
 	elif(len(spec)>1):
 
@@ -73,7 +83,8 @@ def process(sock, msg): #socket object, message
 		if(spec[1] == "INVITE"):
 
 			if spec[2] not in playerDic: #invalid invite - player not in system
-				sock.send("Invited player is not in the system".encode(FORMAT))
+				# sock.send("Invited player is not in the system".encode(FORMAT))
+				pass
 			else: #invite is valid - send invite
 				invitePlayer(spec)
 
@@ -87,12 +98,13 @@ def process(sock, msg): #socket object, message
 
 
 def invitePlayer(spec):
-	playerDic[spec[0]].sock.send("Game invitation sent".encode(FORMAT))
-	print(playerDic[spec[2]].name)
-	playerDic[spec[2]].sock.send(("Recieved game invitation from " + str(spec[0])).encode(FORMAT))
+	# playerDic[spec[0]].sock.send("Game invitation sent".encode(FORMAT))
+	# print(playerDic[spec[2]].name)
+	# playerDic[spec[2]].sock.send(f"NEWINVITE,{str(spec[0])}".encode(FORMAT))
+	send(f"NEWINVITE,{str(spec[0])}", playerDic[spec[2]].sock)
 
 def acceptInvite(spec):
-	playerDic[spec[2]].sock.send(f"{spec[0]} accepted your game invite.".encode(FORMAT))
+	# playerDic[spec[2]].sock.send(f"{spec[0]} accepted your game invite.".encode(FORMAT))
 	gameList.append(Game(spec[2],spec[0])) #add game to gameList
 	print("GAMELIST:")
 	print(gameList.playerOne)
@@ -100,7 +112,8 @@ def acceptInvite(spec):
 
 
 def rejectInvite(spec):
-	playerDic[spec[2]].sock.send(f"{spec[0]} rejected your game invite.".encode(FORMAT))
+	# playerDic[spec[2]].sock.send(f"{spec[0]} rejected your game invite.".encode(FORMAT))
+	pass
 
 def main():
 	print("STARTING server")
