c3297242ca27909ecb555c2cee9e0ccc8504027c
anthonystem <anthony_stem@outlook.com>
2023-04-23 23:10:49 -0400

    Added downloads to website.

--- download.php
+++ download.php
@@ -4,7 +4,21 @@
     include "top.php";
 ?>
         <main class="download">
-                <h1>Sorry, the game is not available to download, yet!</h1>
+            <h2>Game Downloads</h2>
+            <section class="download-section windows">
+                <h3>Windows Download (.exe)</h3>
+                <button><a href="downloads/chesswithfriends.exe" download>Download</a></button>
+            </section>
+
+            <section class="download-section macos">
+                <h3>MacOS Download</h3>
+                <p>Downloading this file will give you a .zip file which you will need to unzip to play.</p>
+                <button><a href="downloads/chesswithfriends.zip" download>Download</a></button>
+            </section>
+
+            <section class="download-section additional-info">
+                <h3>Please Note:</h3>
+                <p>Before you are able to play the game, you must register an account on the Chess With Friends website. You can do so <a href="register.php" class="text-link">here</a>.
         </main>
 
         <?php
